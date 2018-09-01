db5819489638e8fb34d06d52d3efbca2913625db
Daniel Jarai <jaraidaniel@gmail.com>
2018-09-02 04:04:00 +0530

    lettuce: Make the external camera provider ignore internal cameras
    * The external camera provider occupies our camera v4l2 nodes,
      potentially crashing the kernel driver and blocking the camera HAL.
      Unfortunately, there's no easy way to just disable it, so let's just
      tell it to ignore the internal video devices.
    
    Change-Id: I00c67de1b94a1f7eca1bf458d1d0e4de3507aa81
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -57,7 +57,6 @@ PRODUCT_COPY_FILES += \
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbt-vendor
-
 # Display
 PRODUCT_PACKAGES += \
     copybit.msm8916 \
@@ -334,7 +333,10 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
-    camera.device@1.0-impl
+    camera.device@3.2-impl
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/external_camera_config.xml:$(TARGET_COPY_OUT_VENDOR)/etc/external_camera_config.xml
 
 # GPS
 PRODUCT_PACKAGES += \
