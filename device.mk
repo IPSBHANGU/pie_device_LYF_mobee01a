1d800c75ee256da8c25c9c381981309b0ca779c9
Adithya R <gh0strider.2k18.reborn@gmail.com>
2019-01-09 22:56:03 +0530

    LS-5015: Let's roll LOS' MSIM provisioning
    * https://github.com/LineageOS/android/commit/7d33c51bf843f71829fa9dea477d2159fa7dccaa
    [TESTING-NEEDED]
    
    Signed-off-by: AndroiableDroid <androiabledroid@gmail.com>

--- device.mk
+++ device.mk
@@ -373,7 +373,7 @@ PRODUCT_PACKAGES += \
     textclassifier.smartselection.bundle1
 
 # Telephony
-PRODUCT_PACKAGES += telephony-ext
+PRODUCT_PACKAGES += qti-telephony-common
 PRODUCT_BOOT_JARS += telephony-ext
 
 # Thermals
